06e84daf2f0f4afb52fe65950fad7253614e36f0
Zumrut Ekinci Zlabinger <ekinci.z@gmail.com>
2020-07-21 15:24:49 +0200

    Basic

--- src/Zumrut/A1/Main.java
+++ src/Zumrut/A1/Main.java
@@ -1,4 +1,15 @@
 package Zumrut.A1;
 
-public class Main {
+
+public class DivideByZero {
+
+    public static void main(String[] args) {
+        System.out.println("A2 & A2.A3");
+        try {
+            System.out.println(5 / 0);
+        } catch (ArithmeticException e) { //A2 - use Exception, A2.A3 - Use ArithemeticException
+            System.out.println(e);
+        }
+    }
+
 }
